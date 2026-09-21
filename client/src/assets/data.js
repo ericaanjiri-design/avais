34046410c53b54d4728f669216420239f3f3cc39
vistav-rep <eo027007@gmail.com>
2026-09-21 15:50:32 +0300

    Fix jasmine asset import case

--- client/src/assets/data.js
+++ client/src/assets/data.js
@@ -37,7 +37,7 @@ import product_36 from "./product_36.png";
 import product_37 from "./product_37.png";
 import product_38 from "./product_38.png";
 import warm from "./warm wood.jpg";
-import whitejasmine from "./white jasmine.png";
+import whitejasmine from "./White jasmine.png";
 import whiteorchids from "./white orchids.png";
 import oudwood from "./oud wood.png";
 import softpink from "./soft pink.jpg";
